aeef73eef3f05df78c5e60dc96d03beb75d493bf
Pramosh Shrestha <pratha.mero@pm.me>
2020-10-24 20:25:21 +0545

    Axios API services are implemented

---
+++ services/api/AxiosService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { getHeaders } from "./AuthUtils";
+
+const AxiosService = {
+  // The instance used to consume the Login API.
+  login: axios.create({
+    baseURL: process.env.APP_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }),
+
+  // The instance used to consume the protected API services
+  app: axios.create({
+    baseURL: process.env.APP_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }),
+
+  // The instance used to consume cloud services
+  cloud: axios.create({
+    baseURL: process.env.cloud_api_url,
+  }),
+};
+
+// This interceptor is used for changing the "Authorization" header content depending on what request is being made.
+AxiosService.app.interceptors.request.use((config) => {
+  //   if(config.url === "some url"){
+  //     config.headers["Content-Type"] = "multipart/form-data";
+  //   }
+  
+  config.headers = getHeaders();
+});
+
+export default AxiosService;
