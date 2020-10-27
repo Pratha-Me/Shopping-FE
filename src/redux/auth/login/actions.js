0b42a43838fa71fdc7b2f7e571d123bc2e4b0808
Pramosh Shrestha <pratha.mero@pm.me>
2020-10-27 09:04:46 +0545

    Redux and Services are refactored

--- src/redux/auth/login/actions.js
+++ src/redux/auth/login/actions.js
@@ -1,5 +1,12 @@
 import { CHECK_LOGIN, LOGIN_USER_SUCCESSFUL, LOGOUT_USER_SUCCESSFUL, API_FAILED } from './actionTypes';
+<<<<<<< Updated upstream:src/store/auth/login/actions.js
 import * as AuthUtils from '../../../services/api/AuthUtils'
+=======
+import * as AuthHelpers from '../../../helpers/AuthHelpers'
+import * as AuthService from '../../../services/AuthService'
+import jwt from 'jwt-decode';
+import { error } from 'jquery';
+>>>>>>> Stashed changes:src/redux/auth/login/actions.js
 
 const checkLogin = (username, password, history) => {
     return {
@@ -41,10 +48,18 @@ const postLogin = (data, history) => (dispatch) => {
 
         dispatch(loginUserSuccessful(responseData));
         history.push('/');
+<<<<<<< Updated upstream:src/store/auth/login/actions.js
     } catch (error) {
         dispatch(apiError(error))
         console.log(error);
     }
+=======
+
+    }).catch(err => {
+        dispatch(apiError(err))
+        console.log("I am here", err);
+    });
+>>>>>>> Stashed changes:src/redux/auth/login/actions.js
 }
 
 export {postLogin};
---
+++ src/services/AuthService.js
@@ -0,0 +1,99 @@
+import AxiosService from '../api/AxiosService';
+
+// Login Method
+const postLogin = (data) => {
+    return AxiosService.login.post('/user/login', data).catch(err => {
+        let message;
+        if (err.response && err.response.status) {
+            switch (err.response.status) {
+                case 404: message = "Sorry! the page you are looking for could not be found"; break;
+                case 500: message = "Sorry! something went wrong, please contact our support team"; break;
+                case 401: message = "Username or Password is incorrect"; break;
+                default: message = err[1]; break;
+            }
+        }
+        throw message;
+    });
+}
+
+
+/*
+// Register Method
+const postRegister = (data) => {
+    return AxiosService.login.post('/auth/register', data).then(response => {
+        if (response.status >= 200 || response.status <= 299)
+            return response.data;
+        throw response.data;
+    }).catch(err => {
+        let message;
+        if (err.response && err.response.status ) {
+            switch (err.response.status) {
+                case 404: message = "Sorry! the page you are looking for could not be found"; break;
+                case 500: message = "Sorry! something went wrong, please contact our support team"; break;
+                case 401: message = "Invalid credentials"; break;
+                default: message = err[1]; break;
+            }
+        }
+        throw message;
+    });
+
+}
+
+// postForgetPwd 
+const postForgetPwd = (data) => {
+    return axios.post('Look at the postman or the backend code and put an url here', data).then(response => {
+        if (response.status >= 200 || response.status <= 299)
+            return response.data;
+        throw response.data;
+    }).catch(err => {
+        let message;
+        if (err.response && err.response.status ) {
+            switch (err.response.status) {
+                case 400: message = "Unable to send verification code in that email"; break;
+                default: message = err[1]; break;
+            }
+        }
+        throw message;
+    });
+}
+
+const postVerifyCode = (url, data) => {
+    return axios.post(url, data).then(response => {
+        if (response.status >= 200 || response.status <= 299)
+            return response.data;
+        throw response.data;
+    }).catch(err => {
+        console.log(err);
+        let message;
+        if (err.response && err.response.status ) {
+            console.log(err.response.status);
+            switch (err.response.status) {
+                case 400: message = "Code doesn't match"; break;
+                default: message = err[1]; break;
+            }
+        }
+        throw message;
+    });
+}
+
+const postChangePassword = (url, data) => {
+    return axios.post(url, data).then(response => {
+        if (response.status >= 200 || response.status <= 299)
+            return response.data;
+        throw response.data;
+    }).catch(err => {
+        let message;
+        if (err.response && err.response.status ) {
+            switch (err.response.status) {
+                case 404: message = "Sorry! the page you are looking for could not be found"; break;
+                case 500: message = "Sorry! something went wrong, please contact our support team"; break;
+                case 401: message = "Username or Password is incorrect"; break;
+                default: message = err[1]; break;
+            }
+        }
+        throw message;
+    });
+}
+*/
+
+export { postLogin }
