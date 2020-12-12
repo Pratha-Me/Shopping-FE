62845a7f1a0cf77b73cab0affa29c36b2ae8a8ff
Pramosh Shrestha <pratha.mero@pm.me>
2020-12-12 19:06:16 +0545

    Service is added

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -17,22 +17,6 @@ const postLogin = (data) => {
     });
 }
 
-const postForgetPwd= (data) => {
-    // TODO change the url
-    return AxiosService.login.post('/dummy', data).catch(err => {
-        let message;
-        if (err.response && err.response.status) {
-            switch (err.response.status) {
-                case 404: message = "Sorry! the page you are looking for could not be found"; break;
-                case 500: message = "Sorry! something went wrong, please contact our support team"; break;
-                default: message = err[1]; break;
-            }
-        }
-        throw message;
-    })
-}
-
-
 // Register Method
 const postRegister = (data) => {
     return AxiosService.login.post('/auth/register', data).then(response => {
@@ -54,43 +38,12 @@ const postRegister = (data) => {
 
 }
 
-// const postVerifyCode = (url, data) => {
-//     return axios.post(url, data).then(response => {
-//         if (response.status >= 200 || response.status <= 299)
-//             return response.data;
-//         throw response.data;
-//     }).catch(err => {
-//         console.log(err);
-//         let message;
-//         if (err.response && err.response.status ) {
-//             console.log(err.response.status);
-//             switch (err.response.status) {
-//                 case 400: message = "Code doesn't match"; break;
-//                 default: message = err[1]; break;
-//             }
-//         }
-//         throw message;
-//     });
-// }
+const postSendOtp = (data) => {
+    return AxiosService.login.post('/auth/otp', data);
+}
 
-// const postChangePassword = (url, data) => {
-//     return axios.post(url, data).then(response => {
-//         if (response.status >= 200 || response.status <= 299)
-//             return response.data;
-//         throw response.data;
-//     }).catch(err => {
-//         let message;
-//         if (err.response && err.response.status ) {
-//             switch (err.response.status) {
-//                 case 404: message = "Sorry! the page you are looking for could not be found"; break;
-//                 case 500: message = "Sorry! something went wrong, please contact our support team"; break;
-//                 case 401: message = "Username or Password is incorrect"; break;
-//                 default: message = err[1]; break;
-//             }
-//         }
-//         throw message;
-//     });
-// }
+const postForgetPwd = (data) => {
+    return AxiosService.login.post('/auth/change-password', data);
+}
 
-
-export { postLogin, postForgetPwd, postRegister }
+export { postLogin, postSendOtp, postForgetPwd, postRegister }
