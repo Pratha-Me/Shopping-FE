96e67ed1cb00423abb90567df4d61ba2a5e96558
Prem-K-Tamang <ktamangprem@gmail.com>
2020-12-13 07:43:07 +0545

    Edit personal info and address removed

--- src/pages/account/AddressBook.js
+++ src/pages/account/AddressBook.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 
 function AddressBook(props) {
     const [editAddress, setEditAddress ] = useState(false);
-    const [addAddress, setAddAddress] = useState(false);
- 
+    
       
     return (
         <div className="container">
@@ -23,19 +22,8 @@ function AddressBook(props) {
                         <span className="ml-4">+977982325462</span>
                     </div>
                 </div>
-            {!addAddress && !editAddress  &&
-            <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 text-right mt-2 text-right">
-                
-                <a 
-                    onClick={()=>setAddAddress(true)} 
-                    className="btn btn-primary btn-sm  text-white mb-4" 
-                    style={{fontSize:`16px`}}>
-                        Add Address
-                </a>
-            </div>
-            }
 
-            {!addAddress && !editAddress  &&
+            {!editAddress  &&
             <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 pt-3">
                 <div style={{backgroundColor:`#fcfcfc`,padding:`17px`, border:`1px solid #888`, borderRadius:`2px`,fontSize:`16px`, marginBottom:`25vh`}}>
                     <div className="form-group ml-4 mt-4">
@@ -62,79 +50,27 @@ function AddressBook(props) {
                         <a 
                             href="#" 
                             onClick={()=>setEditAddress(true)}
-                            className="btn btn-primary text-white btn-sm pl-4 pr-4 mr-4">
+                            className="btn btn-primary text-white btn-sm pl-4 pr-4">
                                 Edit
                         </a>
-                        <a href="#" className="btn btn-danger text-white btn-sm pl-3 pr-3">Delete</a>
+                        
                     </div>
                 </div>
             </div>
             }
 
 
-            { !editAddress && addAddress &&
-                <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 pt-3"  >
-                 <div style={{backgroundColor:`#fcfcfc`,padding:`17px`, border:`1px solid #888`, borderRadius:`2px`,fontSize:`16px`, marginBottom:`25vh`}}>        
-                    <form method="POST" className="ml-5">
-                        <h5 className="mb-4 mt-3">Add New Address</h5>
-                        <div className="form-group ml-4" style={{"width":"50%"}}>
-                            <label htmlFor="name">
-                                Full Name
-                            </label>
-                            <input type="text" className="form-control mb-3" name="name" id="name" tabIndex="1" autoFocus />
-                        </div>
-                        <div className="form-group ml-4 " style={{"width":"50%"}}>
-                            <label htmlFor="zone">
-                                Zone
-                            </label>
-                            <input type="text" className="form-control mb-3" name="zone" id="zone" tabIndex="2" />
-                        </div>
-                        <div className="form-group ml-4" style={{"width":"50%"}}>
-                            <label htmlFor="city">
-                                City
-                            </label>
-                            <input type="text" className="form-control mb-3" name="city" id="zone" tabIndex="3" required/>
-                        </div>
-                        <div className="form-group ml-4" style={{"width":"50%"}}>
-                            <label htmlFor="address">
-                                Address
-                            </label>
-                            <input type="text" className="form-control mb-3" name="address" id="address" tabIndex="4" />
-                        </div>
-                        <div className="form-group ml-4" style={{"width":"50%"}}>
-                            <label htmlFor="phone_no">
-                                Phone No.
-                            </label>
-                            <input type="text" className="form-control mb-3" name="phone_no" id="phone_no" tabIndex="5" />
-                        </div>
-                       
-                        <div className="form-group ml-4 text-center" style={{"width":"50%"}}>
-                            <input type="submit" className="btn btn-primary btn-sm mb-4 mt-3 mr-4" value="Add Address"/>
-                            <a className="btn btn-danger btn-sm mb-4 mt-3 text-white " onClick={()=>setAddAddress(false)} > Cancel </a>
-                        </div>
-
-                    </form>
-                    </div>
-                </div>
-       
-                }
-
-            { editAddress && !addAddress &&
+            { editAddress &&
                 <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 pt-3"  >
                  <div style={{backgroundColor:`#fcfcfc`,padding:`17px`, border:`1px solid #888`, borderRadius:`2px`,fontSize:`16px`, marginBottom:`25vh`}}>        
                     <form method="POST" className="ml-5">
                         <h5 className="mb-4 mt-3">Update Address</h5>
-                        <div className="form-group ml-4" style={{"width":"50%"}}>
-                            <label htmlFor="name">
-                                Full Name
-                            </label>
-                            <input type="text" className="form-control mb-3" name="name" id="name" tabIndex="1" autoFocus />
-                        </div>
+                        
                         <div className="form-group ml-4 " style={{"width":"50%"}}>
-                            <label htmlFor="zone">
-                                Zone
+                            <label htmlFor="province">
+                                Province
                             </label>
-                            <input type="text" className="form-control mb-3" name="zone" id="zone" tabIndex="2" />
+                            <input type="text" className="form-control mb-3" name="province" id="province" tabIndex="2" />
                         </div>
                         <div className="form-group ml-4" style={{"width":"50%"}}>
                             <label htmlFor="city">
--- src/pages/account/PersonalInfo.js
+++ src/pages/account/PersonalInfo.js
@@ -3,11 +3,9 @@ import { connect } from 'react-redux';
 
 
 function PersonalInfo(props) {
-
-    const [editInfo, setEditInfo] = useState(false);
     const [changePassword, setChangePassword] =  useState(false);
 
-    if (!editInfo && !changePassword) {
+    if (!changePassword) {
         return (
             
             <div className="container">
@@ -20,24 +18,13 @@ function PersonalInfo(props) {
                     <div className="col-xs-6 col-sm-6 col-md-6 col-xl-6 text-right mt-5">
                         <button 
                             onClick={()=>{
-                                setEditInfo(false);
                                 setChangePassword(true);
                             }}
                             className="btn btn-danger btn-sm mr-4 pr-3 pl-3 text-white" 
                             style={{fontSize:`16px`}}>
                                 Change Password
                         </button>
-                        <button
-                            type="button" 
-
-                            onClick={()=>{
-                                setEditInfo(true);
-                                setChangePassword(false);
-                            }} 
-                            className="btn btn-primary btn-sm pl-4 pr-4 text-white" 
-                            style={{fontSize:`16px`}}>
-                                Edit
-                        </button>
+                        
                     </div>
 
                     <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 pt-3">
@@ -66,52 +53,10 @@ function PersonalInfo(props) {
                 </div>
         </div>  
      )
-    } 
-    if(editInfo && !changePassword)
-    {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-6 col-sm-6 col-md-6 col-xl-6 mt-5">
-                        <p style={{fontSize:`17px`, fontWeight:`500`}}>
-                            EDIT INFO
-                        </p>
-                    </div>
-                    <div className="col-xs-6 col-sm-6 col-md-6 col-xl-6 text-right mt-5">
-                        <button
-                            type="button" 
-                            onClick={()=>setEditInfo(false)}
-                            className="btn btn-danger btn-sm pl-4 pr-4 text-white" 
-                            style={{fontSize:`16px`}}>
-                                Close
-                        </button>
-                    </div>
-
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 pt-3">
-                        <div style={{backgroundColor:`#fcfcfc`,padding:`17px`,border:`1px solid #888`, borderRadius:`2px`,fontSize:`16px`, marginBottom:`25vh`}}>
-                            <form method="POST" className="pl-5 pt-3 pr-5 pb-3">
-                                    <div className="form-group">
-                                        <label htmlFor="full_name">Full Name</label>
-                                        <input type="text" className="form-control"  name="full_name" id="full_name" tabIndex="1" autoFocus />
-                                    </div>
-                                    <div className="form-group ">
-                                        <label htmlFor="phone_number" className="mt-2">Phone </label>
-                                        <input type="text" className="form-control" name="phone_number" id="phone_number" tabIndex="2" />
-                                    </div>
-
-                                    <div className="form-group text-center ">
-                                        <input type="submit" value="Save" className="btn btn-primary text-white pl-3 pr-3 mt-3 "/>
-                                    </div>
-                            </form>
-                           
-                        </div>
-                    </div>
-                </div>
-        </div>
-        )
     }
+    
 
-    if(changePassword && !editInfo) {
+    if(changePassword) {
         return (
             <div className="container">
                 <div className="row">
@@ -141,7 +86,6 @@ function PersonalInfo(props) {
                                             type="button" 
                                             onClick={()=>{
                                                     setChangePassword(false);
-                                                    setEditInfo(false);
                                                     }} 
                                             className="btn btn-danger mt-4 btn-sm text-white">
                                                 Cancel
