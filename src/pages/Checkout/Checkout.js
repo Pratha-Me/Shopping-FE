ca8c7bc2e902b9155aa4983018592007a4a83aaa
KhumRaj7Ghimire <khumraj7ghimire@gmail.com>
2021-03-17 16:07:33 +0545

    Checkout Page URL Refactored

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -146,9 +146,13 @@ function Checkout(props) {
         ),
         pid: idProduct && idProduct,
         scd: "EPAYTEST",
-        su: `http://localhost:3000/success?q=su`,
-        fu: `http://localhost:3000/failed?q=fu`,
+        su: `https://supply-inn.com/success?q=su`,
+        fu: `https://supply-inn.com/failed?q=fu`,
       };
+      //       su: `https://supply-inn.com/success?q=su`,
+      // fu: `https://supply-inn.com/failed?q=fu`,
+      //       su: `http://localhost:3000/success?q=su`,
+      // fu: `http://localhost:3000/failed?q=fu`,
       // let path1 = "https://uat.esewa.com.np/epay/transrec";
       // let params1 = {
       //   amt: getSubTotal(carts),
